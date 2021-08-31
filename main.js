e2064da0d2a36214124dbebca887415a1d629f4d
Trijalkamti <82204283+Trijalkamti@users.noreply.github.com>
2021-08-31 12:22:20 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-var SpeechRecognition = window.webkitSpeechRecognition; 
+var SpeechRecognition = window.webkitSpeechRecognition;
 
-var recognition = new SpeechRecognition(); 
+var recognition = new SpeechRecognition();
 
 function start() 
 { document.getElementById("textbox").innerHTML = ""; 
@@ -22,12 +22,17 @@ recognition.onresult = function(event) {
  {
      var synth = window.speechSynthesis;
 
-     speak_data = document.getElementById("textbook").value;
-
+     speak_data = "Taking you Selfie in 5 seconds";
      var utterThis = new SpeechSynthesisUtterance(speak_data);
 
      synth.speak(utterThis);
      Webcam.attach(camera);
+
+     setTimeout(function()
+     {
+         take_snapshot();
+         save();
+     }, 5000);
  }
 
  Webcam.set({
@@ -37,3 +42,18 @@ recognition.onresult = function(event) {
      png_quality:90,
  });
  camera = document.getElementById("camera");
+
+ function take_snapshot()
+ {
+     Webcam.snap(function(data_uri) {
+         document.getElementById("result").innerHTML = '<img id="selfie_image" src="'+data_uri+'">';
+     })
+ }
+
+ function save()
+ {
+     link = document.getElementById("link");
+     image = document.getElementById("selfie_image").src ;
+     link.href = image;
+     link.click();
+ }
